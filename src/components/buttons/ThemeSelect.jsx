8e74d8699027bf08ed4806a74cdd30afd9aee783
Rafael Martinez <rrhhmmtt@gmail.com>
2023-09-06 19:00:26 -0500

    se ajusta queryselector con use effect en el theme, para deploy

--- src/components/buttons/ThemeSelect.jsx
+++ src/components/buttons/ThemeSelect.jsx
@@ -6,24 +6,26 @@ import { createContext, useEffect, useState } from "react";
 
 export default function ThemeSelect() {
   const [themeSelect, setThemeSelect] = useState("dark");
-  let htmlToChange = document.querySelector("html");
-  htmlToChange.setAttribute("data-theme", themeSelect);
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      let htmlToChange = document.querySelector("html");
+      htmlToChange.setAttribute("data-theme", themeSelect);
+    }
+  }, [themeSelect]);
+
   const handleTheme = (e) => {
     if (e.target.checked) {
       setThemeSelect("light");
-      htmlToChange.setAttribute("data-theme", themeSelect);
     } else {
       setThemeSelect("dark");
-      htmlToChange.setAttribute("data-theme", themeSelect);
     }
   };
 
   return (
     <div>
-
-      <BsMoon className="m-1"/>
+      <BsMoon className='m-1' />
       <input type='checkbox' name='checkTheme' id='theme' className='toggle' onChange={handleTheme} />
-      <BsSun className="m-1" />
+      <BsSun className='m-1' />
     </div>
   );
 }
