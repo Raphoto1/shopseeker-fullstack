1a9345b497cad533a95f196272f1b2ee45ac2e06
Rafael Martinez <rrhhmmtt@gmail.com>
2024-02-01 11:06:08 -0500

    se ajusta error de merge

--- next.config.js
+++ next.config.js
@@ -1,35 +1,6 @@
 /** @type {import('next').NextConfig} */
 //https://codingwithmanny.medium.com/3-ways-to-configure-cors-for-nextjs-13-app-router-api-route-handlers-427e10929818
 const nextConfig = {
-<<<<<<< HEAD
-    async headers() {
-        return [{
-            source: "/api/contact/",
-            headers: [
-                {
-                    key: "Access-Control-Allow-Origin",
-                    value: "*",
-                },
-                {
-                    key: "Access-Control-Allow-Methods",
-                    value: "POST,GET", 
-                },
-            ]
-      }]  
-    },
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                port: '',
-                
-            }
-        ]
-    }
-}
-=======
   async headers() {
     return [
       {
@@ -57,6 +28,5 @@ const nextConfig = {
     ],
   },
 };
->>>>>>> dev
 
 module.exports = nextConfig;
