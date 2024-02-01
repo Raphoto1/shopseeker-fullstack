5ff40e3d6c03e6d7b81fbfdf7ca3b57732f71b69
Rafael Martinez <rrhhmmtt@gmail.com>
2024-02-01 11:04:34 -0500

    se ajustan config para merge

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ const nextConfig = {
           { key: "Access-Control-Allow-Methods", value: "POST" },
           {
             key: "Access-Control-Allow-Headers",
-            
             value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
