347e0833d4db50ca817762f3df6b1ec3970f6986
Rafael Martinez <rrhhmmtt@gmail.com>
2025-01-30 12:25:40 -0500

    planteamiento mansonry grid

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,12 @@
 //imports propios
 import ShopsPackProfile from "@/components/extras/ShopsPackProfile";
 import FavoritesCarousel from "@/components/extras/FavoritesCarousel";
-
+import FavoritesBasic from "@/components/extras/FavoritesBasic";
 export default function Home() {
   return (
     <>
-      <FavoritesCarousel />
+      <FavoritesBasic />
+      {/* <FavoritesCarousel /> */}
       <ShopsPackProfile/>
     </>
   );
--- src/config/fetcher.config.js
+++ src/config/fetcher.config.js
@@ -1,3 +1,3 @@
 export const fetcher = async (...args) => await fetch(...args).then((res) => res.json());
 
-export const favoritesUrl = `/api/design?sortField=likes&sortQ=-1&limit=3`;
+export const favoritesUrl = `/api/design?sortField=likes&sortQ=-1&limit=6`;
